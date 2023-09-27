f7f9d4972872b57c057a0aac9dde946529c32652
anarakinson <anarakinson@gmail.com>
2023-09-28 02:26:18 +0300

    start resolving issue 2

--- 02.IP_filter/test/test.cpp
+++ 02.IP_filter/test/test.cpp
@@ -1,11 +1,13 @@
+#define BOOST_TEST_MODULE tests
+
 #include <iostream>
 #include <boost/test/unit_test.hpp>
 
 
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(tests)
 
-BOOST_AUTO_TEST_CASE(test_valid_version) {
+BOOST_AUTO_TEST_CASE(test_test) {
     BOOST_CHECK(1 > 0);
 }
 
