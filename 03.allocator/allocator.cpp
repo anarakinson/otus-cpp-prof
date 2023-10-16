bc954e349f1aadedd15d7433fededf25758718fa
anarakinson <anarakinson@gmail.com>
2023-10-16 17:58:10 +0300

    implement simple allocator

--- 03.allocator/allocator.cpp
+++ 03.allocator/allocator.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <memory>
 #include <map>
+#include <vector>
 #include <string>
 
+
+#define UNUSED(X) (void *)X
 
 
 template <typename T>
@@ -17,18 +20,25 @@ void print_map(const char *name, T &map) {
 }
 
 
+
 template <typename T>
 class Allocator {
 public:
 
     using value_type = T;
 
-    std::shared_ptr<T> pool = nullptr;
+    std::shared_ptr<void> pool;
+    T* pool_start = nullptr;
+    T* pool_end = nullptr;
     static constexpr size_t pool_size = 10;
+    struct deleter {
+        void operator()(void* ptr) {
+            ::operator delete(ptr);
+        }
+    };
 
-    Allocator() noexcept {
-        pool.reset(new T[pool_size]);
-    }
+    Allocator() noexcept : 
+        pool(::operator new (sizeof(T) * pool_size), deleter()) {}
 
     template <typename U>
     Allocator(const Allocator<U> &other) noexcept {
@@ -41,11 +51,21 @@ public:
     }
 
     T *allocate(size_t size) {
-        return pool.get();
+        // UNUSED(size);
+        if (pool_start == nullptr) {
+            pool_start = static_cast<T*>(pool.get());
+        }
+        else {
+            pool_start = pool_end;
+        }
+        pool_end = pool_start + size * sizeof(T);
+        return pool_start; //dummy implementation
     }
 
     void deallocate(T *ptr, size_t size) {
-        //...
+        // UNUSED(size); 
+        // UNUSED(ptr);
+        // ...
     }
 
     template <typename U>
@@ -56,11 +76,6 @@ public:
     using propagate_on_container_copy_assignment = std::true_type;
     using propagate_on_container_move_assignment = std::true_type;
     using propagate_on_container_swap = std::true_type; //UB if std::false_type and a1 != a2;
-
-    // template <typename U>
-    // Allocator operator = (const Allocator<U> &other) noexcept {
-    //     pool = other.pool;
-    // }
 
 };
 
@@ -80,21 +95,26 @@ constexpr bool operator != (const Allocator<T>& a1, const Allocator<U>& a2) noex
 int main() {
 
     std::map<int, int> map{};
-    // std::allocator<std::pair<int, int>> allo{};
     std::map<int, int, std::less<int>, Allocator<std::pair<const int, int>>> map_a{};
 
 
     for (int i = 0; i < 10; ++i) {
         map[i] = i * i;
-    }
-
-    for (int i = 0; i < 10; ++i) {
         map_a[i] = i * i;
     }
 
 
-    print_map("basic map", map);
-    print_map("map with alloc", map_a);
+    std::cout << "map" << ": ";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << map[i] << " ";
+    }
+    std::cout << std::endl;
 
-    
+    std::cout << "map_a" << ": ";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << map_a[i] << " ";
+    }
+    std::cout << std::endl;
+
+
 }
