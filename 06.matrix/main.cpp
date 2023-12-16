260311d43b129e9a5ba6b27b7233de7f3ab33dc0
anarakinson <anarakinson@gmail.com>
2023-12-16 13:51:10 +0300

    first version

--- 06.matrix/main.cpp
+++ 06.matrix/main.cpp
@@ -40,8 +40,8 @@ int main() {
 
     std::cout << "size: " << matrix.size() << "\n" << std::endl;
 
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {    
+    for (int i = 1; i < 9; ++i) {
+        for (int j = 1; j < 9; ++j) {    
             std::cout << matrix[i][j] << " ";
         }
         std::cout << std::endl;
