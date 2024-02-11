748a66bc822e651c918d025f26cc2c510646e449
anarakinson <anarakinson@gmail.com>
2024-02-12 01:22:23 +0300

    make parsing arguments

---
+++ 08.bayan/main.cpp
@@ -0,0 +1,161 @@
+
+#include <boost/program_options.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/format.hpp>
+
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <set>
+#include <vector>
+
+
+
+namespace po = boost::program_options;
+namespace fs = std::filesystem;
+
+po::variables_map parse_args(int argc, char** argv) {
+
+    try {
+        
+        po::options_description descr{"Options"};
+        descr.add_options()
+                ("help,h", "This screen")
+                ("scan,s", po::value<std::vector<std::string>>()->multitoken()->default_value({"."}, "."), "Directories for scan")
+                ("exclude,x", po::value<std::vector<std::string>>()->multitoken(), "Directories excluded from scan")
+                ("depth,d", po::value<bool>()->default_value(0), "Depth of scan: 1-all subdirs, 0-only specified")
+                ("size", po::value<size_t>()->default_value(1), "Minimal size of scanned files (bytes)")
+                ("block", po::value<size_t>()->default_value(4), "Size of block")
+                ("hash", po::value<std::string>()->default_value("md5"), "Type of hashing")
+        ;
+
+        po::variables_map vm;
+        po::store(parse_command_line(argc, argv, descr), vm);
+        po::notify(vm);
+
+
+        if (vm.count("help")){
+            std::cout << descr << std::endl;
+            return {};
+        }
+
+        return vm;
+
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+        return {};
+    }
+    
+}
+
+
+void print_args(po::variables_map &vm) {
+    
+    if (vm.count("scan")){
+        std::cout << "Directories for scan: ";
+        for (auto &val : vm["scan"].as<std::vector<std::string>>()){
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+    if (vm.count("exclude")) {
+        std::cout << "Directories excluded from scan: ";
+        for (auto &val : vm["exclude"].as<std::vector<std::string>>()){
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+    if (vm.count("depth")) {
+        std::cout << "Depth of scan: " << vm["depth"].as<bool>();
+        std::cout << std::endl;
+    }
+    if (vm.count("size")) {
+        std::cout << "Minimal size of scanned files (bytes): " << vm["size"].as<size_t>();
+        std::cout << std::endl;
+    }
+    if (vm.count("block")) {
+        std::cout << "Size of block: " << vm["block"].as<size_t>();
+        std::cout << std::endl;
+    }
+    if (vm.count("hash")) {
+        std::cout << "Type of hashing: " << vm["hash"].as<std::string>();
+        std::cout << std::endl;
+    }
+
+}
+
+
+void path_loop(po::variables_map &vm, std::function<void(fs::directory_entry, fs::directory_entry)> comparator) {
+
+    std::set<fs::directory_entry> checked_obj;
+    for (const std::string &path1 : vm["scan"].as<std::vector<std::string>>()) {
+        for (const auto &obj1 : fs::directory_iterator(path1)) {
+            
+            for (const std::string &path2 : vm["scan"].as<std::vector<std::string>>()) {
+                for (const auto &obj2 : fs::directory_iterator(path2)) {
+                    if ( 
+                        (obj1.is_directory() || 
+                        obj2.is_directory()) ||
+                        obj1.path() == obj2.path()
+                    ) continue;
+                    comparator(obj1, obj2); 
+                }
+                checked_obj.emplace(obj1);
+            }
+
+        }
+    }
+
+}
+
+
+int main(int argc, char** argv) {
+
+    /*----- parse arguments -----*/
+    po::variables_map vm = parse_args(argc, argv);
+    if (vm.size() == 0){
+        return 0;
+    }
+
+    print_args(vm);
+    std::cout << std::endl;
+    /*----- *** -----*/
+
+
+    path_loop(
+        vm, 
+        [](fs::directory_entry a, fs::directory_entry b){ 
+            std::cout << a.path() << " " << b.path() << " " << std::endl; 
+            return; 
+        }
+    );
+
+
+    return 0;
+
+}
+
+
+
+
+/*
+
+
+Утилита должна иметь возможность через параметры командной строки
+указывать
+• директории для сканирования (может быть несколько)
+• директории для исключения из сканирования (может быть несколько)
+• уровень сканирования (один на все директории, 0 - только указанная
+директория без вложенных)
+• минимальный размер файла, по умолчанию проверяются все файлы
+больше 1 байта.
+• маски имен файлов разрешенных для сравнения (не зависят от
+регистра)
+• размер блока, которым производится чтения файлов, в задании этот
+размер упоминается как S
+• один из имеющихся алгоритмов хэширования (crc32, md5 -
+конкретные варианты определить самостоятельно), в задании
+эта функция упоминается как H
+
+
+*/
