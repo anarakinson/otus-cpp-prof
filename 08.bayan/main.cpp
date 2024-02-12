07f332d3d035ab5805a7fd2aff7485c592c11529
anarakinson <anarakinson@gmail.com>
2024-02-13 00:43:49 +0300

    update extend_dirs, exclude_dirs

--- 08.bayan/main.cpp
+++ 08.bayan/main.cpp
@@ -85,24 +85,67 @@ void print_args(po::variables_map &vm) {
 }
 
 
-void path_loop(po::variables_map &vm, std::function<void(fs::directory_entry, fs::directory_entry)> comparator) {
+void print_dirs(const std::vector<std::string> &scan_dirs) {
+    for (auto &d : scan_dirs) {
+        std::cout << d << " ";
+    }
+    std::cout << std::endl;    
+}
 
+
+
+
+void extend_dirs(std::vector<std::string> &dirs) {
+
+    for (const std::string &path : dirs) {
+        for (const auto &obj : fs::directory_iterator(path)) {
+            if (obj.is_directory()) {
+                dirs.emplace_back(obj.path().string());
+            }
+        }
+    }
+
+}
+
+
+void exclude_dirs(std::vector<std::string> &dirs, const std::vector<std::string> &excluded) {
+
+    auto ib = excluded.begin();
+    auto iter = std::remove_if (
+        std::begin(dirs), 
+        std::end(dirs),
+        [&](std::string x) -> bool {
+                while  (ib != excluded.end()) ++ib;
+                return (ib != excluded.end() && *ib == x);
+            }
+        );
+
+}
+
+
+
+
+void path_loop(const std::vector<std::string> &scan_dirs, std::function<void(fs::directory_entry, fs::directory_entry)> comparator) {
+
+    // set to contain already checked paths
     std::set<fs::directory_entry> checked_obj;
-    for (const std::string &path1 : vm["scan"].as<std::vector<std::string>>()) {
+    
+    for (const std::string &path1 : scan_dirs) {
         for (const auto &obj1 : fs::directory_iterator(path1)) {
             
-            for (const std::string &path2 : vm["scan"].as<std::vector<std::string>>()) {
+            for (const std::string &path2 : scan_dirs) {
                 for (const auto &obj2 : fs::directory_iterator(path2)) {
                     if ( 
                         (obj1.is_directory() || 
                         obj2.is_directory()) ||
-                        obj1.path() == obj2.path()
+                        obj1.path() == obj2.path() ||
+                        checked_obj.count(obj2)
                     ) continue;
                     comparator(obj1, obj2); 
                 }
                 checked_obj.emplace(obj1);
             }
-
+        
         }
     }
 
@@ -121,9 +164,21 @@ int main(int argc, char** argv) {
     std::cout << std::endl;
     /*----- *** -----*/
 
+    std::vector<std::string> scan_dirs = vm["scan"].as<std::vector<std::string>>(); 
 
+
+    print_dirs(scan_dirs);
+    // extending directories
+    if (vm["depth"].as<bool>() == true) { extend_dirs(scan_dirs); } 
+    print_dirs(scan_dirs);
+    // excluding directories
+    exclude_dirs(scan_dirs, vm["exclude"].as<std::vector<std::string>>());
+    print_dirs(scan_dirs);
+
+    // go through directories and compare files
     path_loop(
-        vm, 
+        scan_dirs, 
+        // comparator
         [](fs::directory_entry a, fs::directory_entry b){ 
             std::cout << a.path() << " " << b.path() << " " << std::endl; 
             return; 
