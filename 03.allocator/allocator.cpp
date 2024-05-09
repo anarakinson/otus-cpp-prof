ac89efb67624b79dd74395f3f2257ff20aee4fe9
anarakinson <anarakinson@gmail.com>
2024-05-09 17:46:26 +0300

    update pool

--- 03.allocator/allocator.cpp
+++ 03.allocator/allocator.cpp
@@ -33,23 +33,31 @@ void print_map(const char *name, const T &map) {
 
 
 int main() {
+    
+    try {
+        Pool a{sizeof(int)};
+        Pool b = a;
 
-    std::map<int, int> map{};
-    // std::map<int, int, std::less<int>, Mallocator<std::pair<const int, int>>> map_a{};
-    std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>>> map_a{};
+        std::map<int, int> map{};
+        // std::map<int, int, std::less<int>, Mallocator<std::pair<const int, int>>> map_a{};
+        std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>>> map_a{};
 
 
-    for (int i = 0; i < 10; ++i) {
-        std::cout << i << " ";
-        map[i] = factorial(i);
-        map_a[i] = factorial(i);
+        for (int i = 0; i < 10; ++i) {
+            std::cout << i << " ";
+            map[i] = factorial(i);
+            map_a[i] = factorial(i);
+        }
+        std::cout << std::endl;
+
+        std::cout << "output" << std::endl;
+
+        print_map("basic allocator", map);
+        print_map("custom allocator", map_a);
+
+    } catch (std::exception &ex) {
+        std::cout << ex.what() << std::endl;
     }
-    std::cout << std::endl;
-
-    std::cout << "output" << std::endl;
-
-    print_map("basic allocator", map);
-    print_map("custom allocator", map_a);
 
     std::cout << "END" << std::endl;
 
--- 03.allocator/allocator.h
+++ 03.allocator/allocator.h
@@ -8,6 +8,45 @@
 #define UNUSED(X) (void *)X
 
 
+struct Pool {
+    Pool() = delete;
+    Pool(size_t size_of_t) 
+        : own_pool(::operator new (size_of_t * size), deleter())
+    {}
+    Pool(const Pool &other)  = default;
+    // {
+    //     own_pool = other.own_pool;
+    //     start = other.start;
+    //     end = other.end;
+    // }
+    Pool(Pool &&other) = default;
+    // {
+    //     std::swap(own_pool, other.own_pool);
+    //     std::swap(start, other.start);
+    //     std::swap(end, other.end);
+    // }
+    ~Pool() {
+        std::cout << "  >>> Deleted <<<  " << std::endl;
+        start = nullptr;
+        end = nullptr;
+    }
+
+    void* get() { return own_pool.get(); }
+
+    std::shared_ptr<void> own_pool;
+    void* start = nullptr;
+    void* end = nullptr;
+    static constexpr size_t size = 10;
+
+    struct deleter {
+        void operator()(void* ptr) {
+            ::operator delete(ptr);
+        }
+    };
+};
+
+
+
 template <typename T>
 class CustomAllocator {
 public:
@@ -18,22 +57,24 @@ public:
     {
         std::cout << "CONSTRUCTOR: " << typeid(T).name() 
                   << " <<< POOL: " << sizeof(T) * m_pool->size 
-                  << " <<< object size: " << sizeof(*static_cast<T*>(m_pool->own_pool.get())) 
+                  << " <<< object size: " << sizeof(*static_cast<T*>(m_pool->get())) 
                   << std::endl; 
     }
 
     template <typename U>
-    CustomAllocator(const CustomAllocator<U> &) noexcept
+    CustomAllocator(const CustomAllocator<U> &other) noexcept
+    // : m_pool{other.get_pool()}
     {
         std::cout << "COPY CONSTRUCTOR FROM: " << typeid(U).name() 
                   << " <<< POOL: " << sizeof(U) * m_pool->size 
-                  << " <<< object size: " << sizeof(*static_cast<U*>(m_pool->own_pool.get())) 
+                  << " <<< object size: " << sizeof(*static_cast<U*>(m_pool->get())) 
                   << std::endl; 
         std::cout << "COPY CONSTRUCTOR TO: " << typeid(T).name() 
                   <<  " <<< POOL: " << sizeof(T) * m_pool->size 
-                  << " <<< object size: " << sizeof(*static_cast<T*>(m_pool->own_pool.get())) 
+                  << " <<< object size: " << sizeof(*static_cast<T*>(m_pool->get())) 
                   << std::endl; 
-        
+
+        m_pool = other.get_pool();
     }
 
     // template <typename U>
@@ -41,11 +82,11 @@ public:
     // {
     //     std::cout << "COPY CONSTRUCTOR FROM: " << typeid(U).name() 
     //               << " <<< POOL: " << sizeof(U) * m_pool->size 
-    //               << " <<< object size: " << sizeof(*static_cast<U*>(m_pool->own_pool.get())) 
+    //               << " <<< object size: " << sizeof(*static_cast<U*>(m_pool->get())) 
     //               << std::endl; 
     //     std::cout << "COPY CONSTRUCTOR TO: " << typeid(T).name() 
     //               <<  " <<< POOL: " << sizeof(T) * m_pool->size 
-    //               << " <<< object size: " << sizeof(*static_cast<T*>(m_pool->own_pool.get())) 
+    //               << " <<< object size: " << sizeof(*static_cast<T*>(m_pool->get())) 
     //               << std::endl; 
     //     // this->m_pool = m_pool;
     // }
@@ -62,16 +103,16 @@ public:
     T *allocate(size_t size) {
         if (m_pool->start == nullptr) {
             // std::cout << "start: " << size << std::endl;
-            m_pool->start = static_cast<T*>(m_pool->own_pool.get());
+            m_pool->start = static_cast<T*>(m_pool->get());
         }
         else {
             // std::cout << "update: " << size << " <<< " << sizeof(*pool->end) << std::endl;
             m_pool->start = m_pool->end;
         }
-        m_pool->end = m_pool->start + size;
+        m_pool->end = reinterpret_cast<T*>(m_pool->start) + size;
 
         std::cout << "allocated: " << size * sizeof(T) 
-                  << " at: " << m_pool->start << std::endl;
+                  << " at: " << reinterpret_cast<T*>(m_pool->start) << std::endl;
                   
         return reinterpret_cast<T*>(m_pool->start); 
     }
@@ -92,39 +133,14 @@ public:
     using propagate_on_container_swap = std::true_type; //UB if std::false_type and a1 != a2;
 
 private:
-
-    struct Pool {
-        Pool() 
-            : own_pool(::operator new (sizeof(T) * size), deleter())
-        {}
-        Pool(const Pool&) 
-            : own_pool(::operator new (sizeof(T) * size), deleter())
-        {}
-        Pool(Pool&&) 
-            : own_pool(::operator new (sizeof(T) * size), deleter())
-        {}
-        ~Pool() {
-            std::cout << ">>>Deleted<<<" << sizeof(T) << std::endl;
-            // delete own_pool;
-            start = nullptr;
-            end = nullptr;
-        }
-
-        std::shared_ptr<void> own_pool;
-        T* start = nullptr;
-        T* end = nullptr;
-        static constexpr size_t size = 10;
-    };
-
-    struct deleter {
-        void operator()(void* ptr) {
-            ::operator delete(ptr);
-        }
-    };
     
-    std::shared_ptr<Pool> m_pool = std::make_shared<Pool>(Pool{});
-    // Pool* m_pool = new Pool{};
+    std::shared_ptr<Pool> m_pool = std::make_shared<Pool>(Pool{sizeof(T)});
+    // Pool* m_pool = new Pool{sizeof(T)};
     // inline static Pool m_pool{};
+
+public:
+    std::shared_ptr<Pool> get_pool() const { return m_pool; }
+
 
 };
 
@@ -141,53 +157,53 @@ constexpr bool operator != (const CustomAllocator<T>& a1, const CustomAllocator<
 
 
 
-template<class T>
-struct Mallocator
-{
-    typedef T value_type;
+// template<class T>
+// struct Mallocator
+// {
+//     typedef T value_type;
  
-    Mallocator() {
-        std::cout << sizeof(T) << std::endl;
-    }
+//     Mallocator() {
+//         std::cout << sizeof(T) << std::endl;
+//     }
  
-    template<class U>
-    constexpr Mallocator(const Mallocator <U>&) noexcept {
-        std::cout << sizeof(U) << std::endl;
-        std::cout << sizeof(T) << std::endl;
-    }
+//     template<class U>
+//     constexpr Mallocator(const Mallocator <U>&) noexcept {
+//         std::cout << sizeof(U) << std::endl;
+//         std::cout << sizeof(T) << std::endl;
+//     }
  
-    [[nodiscard]] T* allocate(std::size_t n)
-    {
-        if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
-            throw std::bad_array_new_length();
+//     [[nodiscard]] T* allocate(std::size_t n)
+//     {
+//         if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+//             throw std::bad_array_new_length();
  
-        if (auto p = static_cast<T*>(std::malloc(n * sizeof(T))))
-        {
-            report(p, n);
-            return p;
-        }
+//         if (auto p = static_cast<T*>(std::malloc(n * sizeof(T))))
+//         {
+//             report(p, n);
+//             return p;
+//         }
  
-        throw std::bad_alloc();
-    }
+//         throw std::bad_alloc();
+//     }
  
-    void deallocate(T* p, std::size_t n) noexcept
-    {
-        report(p, n, 0);
-        std::free(p);
-    }
-private:
-    void report(T* p, std::size_t n, bool alloc = true) const
-    {
-        std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(T) * n
-                  << " bytes at " << std::hex << std::showbase
-                  << reinterpret_cast<void*>(p) << std::dec << '\n';
-    }
-};
+//     void deallocate(T* p, std::size_t n) noexcept
+//     {
+//         report(p, n, 0);
+//         std::free(p);
+//     }
+// private:
+//     void report(T* p, std::size_t n, bool alloc = true) const
+//     {
+//         std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(T) * n
+//                   << " bytes at " << std::hex << std::showbase
+//                   << reinterpret_cast<void*>(p) << std::dec << '\n';
+//     }
+// };
  
-template<class T, class U>
-bool operator==(const Mallocator <T>&, const Mallocator <U>&) { return true; }
+// template<class T, class U>
+// bool operator==(const Mallocator <T>&, const Mallocator <U>&) { return true; }
  
-template<class T, class U>
-bool operator!=(const Mallocator <T>&, const Mallocator <U>&) { return false; }
+// template<class T, class U>
+// bool operator!=(const Mallocator <T>&, const Mallocator <U>&) { return false; }
  
 
